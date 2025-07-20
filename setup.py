ec5728ce8b9cbf981fe288c93790a54171d2d1e5
unknown <siddharthkotak280@gmail.com>
2025-07-20 17:04:43 +0530

    utils,exception and logger added

--- setup.py
+++ setup.py
@@ -1,26 +1,39 @@
 import setuptools
 
-with open("README.md","r",encoding="utf-8") as f:
+with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-__version__ = "0.0.0"
-REPO_NAME="Cancer-Prediction"
-AUTHOR_USER_NAME="SiddhantKotak"
-SRC_REPO="chestCancerClassifier"
-AUTHOR_EMAIL="siddharthkotak280@gmail.com"
+__version__ = "0.1.0"
+REPO_NAME = "Cancer-Prediction"
+AUTHOR_USER_NAME = "SiddhantKotak"
+SRC_REPO = "chestCancerClassifier"
+AUTHOR_EMAIL = "siddharthkotak280@gmail.com"
 
 setuptools.setup(
     name=SRC_REPO,
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description="A small python package for CNN app",
+    description="A small python package for CNN-based cancer prediction",
     long_description=long_description,
-    long_description_content="text/markdown",
+    long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
-        "Bug Tracker":f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
-    package_dir={"":"src"},
-    packages=setuptools.find_packages(where="src")
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=[
+        "tensorflow==2.12.0",
+        "numpy",
+        "pandas",
+        # Add more as needed
+    ],
+    python_requires=">=3.8",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    license="MIT",
 )
